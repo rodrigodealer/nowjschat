70e207b525e1925963b702ba163988403da48e52
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2011-05-28 11:46:53 -0300

    Organizando arquivos do projeto

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ server = http.createServer(function(req, res){
   var path = url.parse(req.url).pathname;
   switch (path){
     case '/':
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write('<h1>Welcome. Try the <a href="/cliente.html">chat</a> example.</h1>');
+      res.writeHead(200, 'text/html');
+      res.write('<h1>Welcome. Try the <a href="/cliente">chat</a> example.</h1>');
       res.end();
       break;
-    case '/cliente.html':
-      fs.readFile(__dirname + path, function(err, data){
+    case '/cliente':
+      fs.readFile(__dirname + '/view' +  path + '.html', function(err, data){
         if (err) return send404(res);
-        res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'})
+        res.writeHead(200, 'text/html')
         res.write(data, 'utf8');
         res.end();
       });
